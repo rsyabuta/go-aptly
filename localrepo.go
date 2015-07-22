93a1a2889ac89c180a501e806aa2ad2dfd4bdc6f
Marin <marin@stayke.net>
2015-07-22 13:41:11 -0700

    make PackageCollection Sortable

--- localrepo.go
+++ localrepo.go
@@ -57,6 +57,23 @@ type Package struct {
 	Version       string `json:"Version"`
 }
 
+func (pl *PackageCollection) Len() int {
+	return len(pl.Packages)
+
+}
+
+func (pl *PackageCollection) Swap(a, b int) {
+	pl.Packages[a], pl.Packages[b] = pl.Packages[b], pl.Packages[a]
+}
+
+func (pl *PackageCollection) Less(a, b int) bool {
+	if pl.Packages[a].Package == pl.Packages[b].Package {
+		return pl.Packages[a].Package < pl.Packages[b].Package
+	}
+
+	return pl.Packages[a].Version < pl.Packages[b].Version
+}
+
 type PackageCollection struct {
 	Packages []Package
 }
